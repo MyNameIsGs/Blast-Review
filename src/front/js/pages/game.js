49a329ff7f3245fb27944f537811782f17cb1a0c
Sebastián Cardona <sebascarmun@gmail.com>
2023-02-03 19:36:06 -0400

    caja comentarios en la vista game

---
+++ src/front/js/pages/game.js
@@ -0,0 +1,6 @@
+import React, { useContext } from "react";
+export const Game = ()=>{
+    return (<div className="container commentBox pt-3"><div className="mb-3">
+    <textarea placeholder="Write a Comment" className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    <div className="d-flex justify-content-end pt-1"><button type="button" className="btn btn-outline-success">Send</button></div></div></div>)
+}
