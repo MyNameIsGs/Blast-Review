ef2c39e286448e7ad6b7b396c8628eaa0061d430
Sebastián Cardona <sebascarmun@gmail.com>
2023-02-03 20:05:55 -0400

    Calificación por números

--- src/front/js/pages/game.js
+++ src/front/js/pages/game.js
@@ -1,6 +1,23 @@
 import React, { useContext } from "react";
-export const Game = ()=>{
-    return (<div className="container commentBox pt-3"><div className="mb-3">
-    <textarea placeholder="Write a Comment" className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-    <div className="d-flex justify-content-end pt-1"><button type="button" className="btn btn-outline-success">Send</button></div></div></div>)
-}
+export const Game = () => {
+  return (
+    <div className="container commentBox pt-3">
+      <div className="mb-3">
+        <div>
+          <input type="number" min={1} max={10} ></input>
+        </div>
+        <textarea
+          placeholder="Write a Comment"
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+        ></textarea>
+        <div className="d-flex justify-content-end pt-1">
+          <button type="button" className="btn btn-outline-success">
+            Send
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
