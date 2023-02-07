69478f39c003b405ce4d247a9f957a30dbd5316d
Sebastián Cardona <sebascarmun@gmail.com>
2023-02-06 21:39:53 -0400

    Imagen añadida, interfaz levemente mejorada

--- src/front/js/pages/game.js
+++ src/front/js/pages/game.js
@@ -1,37 +1,57 @@
 import React, { useContext } from "react";
 export const Game = () => {
   return (
-    <div>
-        <div className="container d-flex justify-content-end"> 
-        <div className="p-3"> 9/10</div>
-        
-        
-        <div className="p-3">8/10</div>
-        </div>
-      <div className="container" >
-        <p>
-          The Legend of Zelda: Breath of the Wild es la nueva aventura de acción
-          de Nintendo para Wii U y Nintendo Switch que nos presenta el título
-          más ambicioso de la saga con un mundo abierto por explorar y en el que
-          realizar todo tipo de acciones como escalar además de otras clásicas
-          como nadar o montar a caballo.
-        </p>
+    <div className="d-flex">
+      <div className="cover ps-3">
+        <h2 className="text-center">The Legend of Zelda</h2>
+        <img
+          className="imageCover"
+          src="https://upload.wikimedia.org/wikipedia/en/c/c6/The_Legend_of_Zelda_Breath_of_the_Wild.jpg"
+        ></img>
       </div>
-      <div className="container commentBox pt-3">
-        <div className="mb-3">
-          <div>
-            <input type="number" min={1} max={10}></input>
+      <div className="information">
+        <div className=" container d-flex ">
+          <div className="pt-3 score ">
+            <p className="text-center mb-0 mx-4"> Puntaje Casual 👶</p>
+
+            <p className="text-center mt-0"> 6/10</p>
           </div>
-          <textarea
-            placeholder="Write a Comment"
-            className="form-control"
-            id="exampleFormControlTextarea1"
-            rows="3"
-          ></textarea>
-          <div className="d-flex justify-content-end pt-1">
-            <button type="button" className="btn btn-outline-success">
-              Send
-            </button>
+          <div className="pt-3 score ">
+            <p className="text-center mb-0 mx-4"> Puntaje Habitual 💪</p>
+            <p className="text-center mt-0"> 9/10</p>
+          </div>
+        </div>
+
+        <div className="container">
+          <p>
+            The Legend of Zelda: Breath of the Wild es la nueva aventura de
+            acción de Nintendo para Wii U y Nintendo Switch que nos presenta el
+            título más ambicioso de la saga con un mundo abierto por explorar y
+            en el que realizar todo tipo de acciones como escalar además de
+            otras clásicas como nadar o montar a caballo.
+          </p>
+        </div>
+        <div className="ps-3">
+          <div className="container commentBox pt-3 ">
+            <div className="mb-3">
+              <div className="pb-2">
+                <input type="number" min={1} max={10}></input>
+              </div>
+              <textarea
+                placeholder="Write Your Opinion About This Game"
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+              ></textarea>
+              <div className="d-flex justify-content-end pt-1 pb-1">
+                <button
+                  type="button"
+                  className="btn btn-outline-success btn-sm"
+                >
+                  Send
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
