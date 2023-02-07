0c2907e82d67600995a14d32d49cc63e8e263abd
gustavomm19 <gustavomm199@gmail.com>
2023-02-07 05:14:15 +0000

    register user done

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,6 +5,8 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
+import { Signup } from "./pages/signup";
+import { Login } from "./pages/login";
 import { Game } from "./pages/game";
 import injectContext from "./store/appContext";
 
@@ -25,6 +27,8 @@ const Layout = () => {
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
+                        <Route element={<Signup />} path="/signup" />
+                        <Route element={<Login />} path="/login" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<Game />} path ="/game"/>
                         <Route element={<h1>Not found!</h1>} />
